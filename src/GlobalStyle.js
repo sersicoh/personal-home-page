fd7f2f324c9f8bc5e6a65336300bf9af79f249e9
Wojciech Szymczak <sersicoh@gmail.com>
2022-08-25 16:29:49 +0200

    added global style to project

---
+++ src/GlobalStyle.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    html {
+    box-sizing: border-box;
+    }
+
+    *, ::after, ::before {
+    box-sizing: initial;
+    }
+
+    body{
+    margin: 20px;
+    background: #eee;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+    }
+`;
