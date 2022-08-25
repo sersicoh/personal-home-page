0d7d7f17753b8647a21f26f193c5ff0d9696fa42
Wojciech Szymczak <sersicoh@gmail.com>
2022-08-25 16:30:09 +0200

    remove not used content

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Ta strona jest w trakcie tworzenia, zajrzyj za jakiś czas 😉
         </p>
-      </header>
-    </div>
+
+        
   );
 }
 
