ee12e8067f06081d6a6aaaa49a9e12fc545c99e9
Wojciech Szymczak <sersicoh@gmail.com>
2022-10-22 15:08:53 +0200

    add few colors and settings

--- src/theme.js
+++ src/theme.js
@@ -4,9 +4,12 @@ export const theme = {
         black: "#000000",
         white: "#ffffff",
         grey: "#808080",
+        staleGray: "#6E7E91",
+        scienceBlue: "#0366D6",
 
     },
     breakpoint: {
+        normalMax: 1300,
         mobileMax: 767,
         tabletMax: 960,
     },
