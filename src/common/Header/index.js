acb2b493c186f67f4c7eafc4ea1b2d686926ef43
Wojciech Szymczak <sersicoh@gmail.com>
2022-11-13 15:36:38 +0100

    add theme switcher to header

--- src/common/Header/index.js
+++ src/common/Header/index.js
@@ -1,14 +1,25 @@
-import {Description, HireButton, HireButtonIcon, HireButtonText, Name, Picture, Who, Wrapper} from "./styles";
-
+import {
+    Description,
+    HireButton,
+    HireButtonIcon,
+    HireButtonText,
+    Name,
+    Picture,
+    SwitchTheme,
+    Who,
+    Wrapper
+} from "./styles";
+import {ReactComponent as Message} from "./Message.svg";
 export const Header = () => (
     <Wrapper>
         <Picture src={"https://ca.slack-edge.com/T01RG0VJ7FF-U03F2JXDUCE-d38ac6966a72-512"} alt={"Wojtek Szymczak"}/>
+        <SwitchTheme>Tu będzie przycisk</SwitchTheme>
         <Who>this is</Who>
         <Name>Wojtek Szymczak</Name>
         <Description>I’m a passionate Frontend Developer in love with React, currently looking for new job
             opportunities</Description>
         <HireButton>
-            <HireButtonIcon>D </HireButtonIcon>
+            <HireButtonIcon><Message/></HireButtonIcon>
             <HireButtonText>Hire Me</HireButtonText>
         </HireButton>
     </Wrapper>
--- src/common/Header/styles.js
+++ src/common/Header/styles.js
@@ -5,7 +5,9 @@ export const Wrapper = styled.div`
   grid-auto-columns: auto 1fr;
   grid-column-gap: 66px;
   align-items: center;
+  margin-top: 115px;
   grid-template-areas:
+  "picture switchTheme"
   "picture who"
   "picture name"
   "picture description"
@@ -15,10 +17,18 @@ export const Wrapper = styled.div`
 export const Picture = styled.img`
   grid-area: picture;
   max-width: 400px;
-  margin-top: 115px;
-  border-radius: 50%;
+  border-radius: 50%; 
 `;
 
+export const SwitchTheme = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  font-weight: 700;
+  font-size: 12px;
+  line-height: 130%;
+  text-transform: uppercase;
+  color: ${({theme}) => theme.colors.staleGray};
+`;
 export const Who = styled.div`
   grid-area: who;
   font-weight: 700;
@@ -26,15 +36,15 @@ export const Who = styled.div`
   line-height: 130%;
   text-transform: uppercase;
   color: ${({theme}) => theme.colors.staleGray};
-  margin-bottom: 12px;
-  margin-top: 183px
+  //margin-bottom: 12px;
+  margin-top: 68px
 `;
 
 export const Name = styled.div`
   grid-area: name;
   font-size: 38px;
   font-weight: 900;
-  margin-bottom: 35px;
+  color: ${({theme}) => theme.colors.mineShaft};
 `;
 
 export const Description = styled.div`
@@ -43,21 +53,23 @@ export const Description = styled.div`
   line-height: 140%;
   letter-spacing: 0.05em;
   color: ${({theme}) => theme.colors.staleGray};
-
 `;
-// TODO zrobić przycisk HireButton, jak na filmie z kursu - obramowanie bedzie trzeba wylaczyc
+
 export const HireButton = styled.button`
+  grid-area: hireButton;
   max-width: 154px;
   max-height: 49px;
-  grid-area: hireButton;
+  border: 1px solid rgba(209, 213, 218, 0.3);
+  border-radius: 4px;
   display: flex;
   justify-content: center;
   align-items: center;
+  margin-bottom: 20px;
   background: ${({theme}) => theme.colors.scienceBlue};
 `;
 export const HireButtonIcon = styled.div`
   color: ${({theme}) => theme.colors.white};
-  
+
 `;
 export const HireButtonText = styled.div`
   padding: 12px 16px;
@@ -66,3 +78,4 @@ export const HireButtonText = styled.div`
   letter-spacing: 0.05em;
   color: ${({theme}) => theme.colors.white};
 `;
+
