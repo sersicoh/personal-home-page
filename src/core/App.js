05b189f011fa1107185f06717c68e2152c1171c1
Wojciech Szymczak <sersicoh@gmail.com>
2022-11-13 15:35:12 +0100

    add Skills section to app

--- src/core/App.js
+++ src/core/App.js
@@ -1,12 +1,15 @@
 // import {Content} from "../common/Content/Content";
 import {Header} from "../common/Header";
 import {Content} from "../common/Content/styles";
+import {Skills} from "../features/Skills";
 
 function App() {
 
     return (
         <Content>
             <Header/>
+            <Skills/>
+            <Skills/>
         </Content>
 
     );
